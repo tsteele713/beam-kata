8edf469dc20e08622d0ffbb5d00cac859149b1cd
tyler steele <steele.tyler13@gmail.com>
2022-05-01 09:41:28 -0400

    Fixing patient service unit test

--- src/app/services/patient.service.spec.ts
+++ src/app/services/patient.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { PatientService } from './patient.service';
 
 describe('PatientService', () => {
   let service: PatientService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        HttpClient,
+      ]
+    });
     service = TestBed.inject(PatientService);
   });
 
