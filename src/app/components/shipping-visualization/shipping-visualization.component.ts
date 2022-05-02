95bc2e0a468b89c78c0c8558928eb6f182e26635
tyler steele <steele.tyler13@gmail.com>
2022-05-01 20:34:22 -0400

    Refactoring shipping visualization component to reduce repeated code

--- src/app/components/shipping-visualization/shipping-visualization.component.ts
+++ src/app/components/shipping-visualization/shipping-visualization.component.ts
@@ -29,57 +29,24 @@ export class ShippingVisualizationComponent implements OnInit {
     this.patientService.retrievePatientsForShipping().pipe(first()).subscribe((familyPatientList) => {
       this.starterBoxCount = Math.ceil(familyPatientList.length / 2);
       this.colorSortedList = familyPatientList.sort((a, b) => (a.brush_color < b.brush_color) ? -1 : 1);
-      this.displayBoxes.next(this.isFromStarter ? this.createStarterBoxes() : this.createRefillBoxes());
+      this.displayBoxes.next(this.isFromStarter ? this.createBoxVisualization(2, this.isFromStarter) : this.createBoxVisualization(4, this.isFromStarter));
     });
   }
 
-  private createStarterBoxes(): Box[]{
-      let boxes: Box[] = [];
-      for (let i = 0; i < this.colorSortedList.length; i = i + 2) {
-        let box: Box = { brushes: [] };
-          let firstPatient = this.colorSortedList[i];
-          let nextPatient = i + 1 < this.colorSortedList.length ? this.colorSortedList[i + 1] : null;
-
-          if (nextPatient && firstPatient.brush_color === nextPatient.brush_color) {
-            box.brushes.push({
-              color: firstPatient.brush_color,
-              replacementBrushCount: 2,
-              brushCount: 2
-            });
-          } else {
-              box.brushes.push(this.defaultBrush(firstPatient.brush_color, true));
-            if (nextPatient) {
-              box.brushes.push(this.defaultBrush(nextPatient.brush_color, true));
-            }
-          }
-          boxes.push(box);
-      }
-      return boxes;
-  }
-
-  private createRefillBoxes(): Box[] {
+  private createBoxVisualization(sliceLength: number, isStarterBox: boolean): Box[] {
     let boxes: Box[] = [];
-    for (let i = 0; i < this.colorSortedList.length; i += 4) {
+    for (let i = 0; i < this.colorSortedList.length; i += sliceLength) {
         let brushes: Brush[] = [];
-        const chunk = this.colorSortedList.slice(i, i + 4);
+        const chunk = this.colorSortedList.slice(i, i + sliceLength);
         const unique = [...new Set(chunk.map(item => item.brush_color))];
         unique.forEach((element) => {
           let colorChunk = chunk.filter((patient) => patient.brush_color === element);
-          brushes.push({ color: element, replacementBrushCount: colorChunk.length, brushCount: 0});
+          let brushCount = isStarterBox ? colorChunk.length : 0;
+          brushes.push({ color: element, replacementBrushCount: colorChunk.length, brushCount: brushCount});
         });
         boxes.push({brushes: brushes});
 
     }
     return boxes;
   }
-
-  private defaultBrush(color: string, isStarterBox: boolean) {
-    let initialBrushCount = isStarterBox ? 1 : 0;
-    return {
-      color: color,
-      replacementBrushCount: 1,
-      brushCount: initialBrushCount
-    }
-  }
-
 }
